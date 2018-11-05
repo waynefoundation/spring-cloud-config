fb6cfdd62f19a846d86d7dcbca30d89ecadf1daf
Liam Beeton <liam.beeton@gmail.com>
2018-11-05 18:33:07 +0200

    added project artifact and version fix

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build --build-arg url=https://github.com/waynefoundation/spring-cloud-config.git \
---build-arg project=config-server \
---build-arg artifactid=config-server \
+--build-arg project=spring-cloud-config \
+--build-arg artifactid=spring-cloud-config \
 --build-arg version=1.0 \
--t wayneenterprises/config-server - < Dockerfile
+-t wayneenterprises/spring-cloud-config - < Dockerfile
